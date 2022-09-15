ddd443ba2b4378539b944beb069192f730686a7a
Troy Wilson <Troy.Wilson.Anthony@gmail.com>
2022-09-15 10:59:15 -0400

    Did too much stuff

---
+++ MovieDatabaseEFConsole/Models/Movie.cs
@@ -0,0 +1,84 @@
+﻿namespace MovieDatabaseEFConsole.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Genre { get; set; } = null!;
+
+        public Movie(string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
+        }
+
+
+
+        //SearchByGenre: This method will return a list of Movie instances that match the genre.
+        public static void SearchByGenre()
+        {
+
+            Console.Write("Please enter the genre you would like to see: ");
+
+            string userInput = Console.ReadLine();
+            using(var context = new MovieDBContext())
+            {
+                if(!string.IsNullOrWhiteSpace(userInput))
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Movie genre = context.Movies.Where(s => s.Genre.StartsWith(userInput)).First();
+                        Console.WriteLine($"List of {genre.Genre} movies.");
+                        foreach(var m in context.Movies)
+                        {
+                            if(m.Genre == genre.Genre)
+                            {
+                                Console.WriteLine($"{m.Title}");
+                            }
+                        }
+                        return;
+                    }
+                    catch(Exception)
+                    {
+                        Console.WriteLine($"Sorry {userInput} is not a valid genre.");
+                        return;
+                    } 
+                }
+                Console.WriteLine("Sorry that is not a valid entry.");
+            }
+        }
+
+        //SearchByTitle: This method will return a list of Movie instances that match the title.
+        public static void SearchByTitle()
+        {
+            Console.Write("Please enter the title you are looking for: ");
+
+            string userInput = Console.ReadLine();
+            using(var context = new MovieDBContext())
+            {
+                if(!string.IsNullOrWhiteSpace(userInput))
+                {
+                    try
+                    {
+                        Console.Clear();               
+                        foreach(var m in context.Movies)
+                        {
+                            if(m.Title.StartsWith(userInput ,StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine($"{m.Title} {m.Genre}");
+                            }
+                        }
+                        return;
+                    }
+                    catch(Exception)
+                    {
+                        Console.WriteLine($"Sorry {userInput} is not a valid genre.");
+                        return;
+                    }
+                }
+                Console.WriteLine("Sorry that is not a valid entry.");
+            }
+        }
+    }
+}
--- MovieDatabaseEFConsole/Program.cs
+++ MovieDatabaseEFConsole/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace MovieDatabaseEFConsole
+﻿using MovieDatabaseEFConsole.Models;
+
+namespace MovieDatabaseEFConsole
 {
     internal class Program
     {
         static void Main()
         {
+            CreateMovies();
+            //Movie.SearchByGenre();
+            Movie.SearchByTitle();
+
+            static void CreateMovies()
+            {
+                List<Movie> movieList = new();
+
+                using(var context = new MovieDBContext())
+                {
+                    var AllMovies = context.Movies.ToList();
+
+
+                    if(AllMovies.Count == 0)
+                    {
+                        MakeMovieList(movieList);
+
+                        foreach(var movie in movieList)
+                        {
+                            context.Movies.Add(movie);
+                        }
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        public static void MakeMovieList(List<Movie> movieList)
+        {
+            string[] lines = File.ReadAllLines(@".\MovieList.txt");
+
+            foreach(string line in lines)
+            {
+                string[] entries = line.Split(',');
+                Movie newMovie = new(entries[0], entries[1]);
+                movieList.Add(newMovie);
+            }
+            movieList = movieList.OrderBy(x => x.Title).ThenBy(x => x.Genre).ToList();
         }
     }
 }
